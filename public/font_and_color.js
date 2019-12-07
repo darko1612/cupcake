287f763324fff1d7fb0eb1394b7932eca1992a89
darko krajnovic <darkokrajnovic@gmail.com>
2019-12-07 23:15:21 +0100

    Home template update

--- public/font_and_color.js
+++ public/font_and_color.js
@@ -2,12 +2,15 @@ function updateSize() {
     document.getElementById('pre_title').style.fontSize = document.getElementById('size').value + "px";
     document.getElementById('title').style.fontSize = document.getElementById('title_size').value + "px";
     document.getElementById('section1_pre_title').style.fontSize = document.getElementById('section1_pre_title_size').value + "px";
+    document.getElementById('section1_title').style.fontSize = document.getElementById('section1_title_size').value + "px";
+
 }
 
 function updateColor() {
     document.getElementById('pre_title').style.color = document.getElementById('color').value;
     document.getElementById('title').style.color = document.getElementById('title_color').value;
-
+    document.getElementById('section1_pre_title').style.color = document.getElementById('section1_pre_title_color').value;
+    document.getElementById('section1_title').style.color = document.getElementById('section1_title_color').value;
 }
 
 var fonts = ["Montez", "Lobster", "Josefin Sans", "Shadows Into Light", "Pacifico", "Amatic SC", "Orbitron", "Rokkitt", "Righteous", "Dancing Script", "Bangers", "Chewy", "Sigmar One", "Architects Daughter", "Abril Fatface", "Covered By Your Grace", "Kaushan Script", "Gloria Hallelujah", "Satisfy", "Lobster Two", "Comfortaa", "Cinzel", "Courgette"];
@@ -37,6 +40,13 @@ for (var a = 0; a < fonts.length; a++) {
     select_section1_pre_title.add(opt);
 }
 
+for (var a = 0; a < fonts.length; a++) {
+    let opt = document.createElement('option');
+    opt.value = opt.innerHTML = fonts[a];
+    opt.style.fontFamily = fonts[a];
+    select_section1_title.add(opt);
+}
+
 function fontChange() {
     var x = document.getElementById("select").selectedIndex;
     var y = document.getElementById("select").options;
@@ -55,11 +65,20 @@ function fontTitleChange() {
 
 fontTitleChange();
 
-function fontSection1TitleChange() {
+function fontSection1PreTitleChange() {
     var x = document.getElementById("select_section1_pre_title").selectedIndex;
     var y = document.getElementById("select_section1_pre_title").options;
     document.body.insertAdjacentHTML("beforeend", "<style> .section1_pre_title{ font-family:'" + y[x].text + "';}" +
         "#select_section1_pre_title{font-family:'" + y[x].text + "';</style>");
+}
+
+fontSection1PreTitleChange();
+
+function fontSection1TitleChange() {
+    var x = document.getElementById("select_section1_title").selectedIndex;
+    var y = document.getElementById("select_section1_title").options;
+    document.body.insertAdjacentHTML("beforeend", "<style> .section1_title{ font-family:'" + y[x].text + "';}" +
+        "#select_section1_title{font-family:'" + y[x].text + "';</style>");
 }
 
 fontSection1TitleChange();
--- resources/views/editable_templates/home.blade.php
+++ resources/views/editable_templates/home.blade.php
@@ -1,19 +1,23 @@
 <span>
     <p>Izaberi sliku za pozadinu:</p>
-    <input type="file" name="background" id="background">
+    <input type="file" name="background" id="background" class="btn btn-dark">
 </span>
 <span class="site-heading text-center d-none d-lg-block">
     <span class="site-heading-upper text-primary mb-3">
-        <input type="text" placeholder="Manji naslov" name="pre_title" value="{{$page->pre_title ?? ''}}" class="text col-sm-4" id="pre_title">
-        <select id='select' onChange="fontChange();" class="col-sm-3"></select>
-        <input type="number" id="size" min="0" max="70" value="30" onchange="updateSize()" class="col-sm-2"/>
-        <input type="color" onchange="updateColor()" id="color" class="col-sm-1"/>
+        <input type="text" placeholder="Prva linija naslova" name="pre_title" value="{{$page->pre_title ?? ''}}"
+               class="text col-sm-4" id="pre_title" title="Izmeni prvu liniju naslova">
+        <select id='select' onChange="fontChange();" class="col-sm-3" title="Promeni font"></select>
+        <input type="number" id="size" min="0" max="70" value="30" onchange="updateSize()" class="col-sm-2"
+               title="Promeni veličinu"/>
+        <input type="color" onchange="updateColor()" id="color" class="col-sm-1" title="Promeni boju"/>
     </span>
     <span class="site-heading-upper text-primary">
-        <input type="text" placeholder="Veci naslov" name="title" value="{{$page->title ?? ''}}" class="title col-sm-4" id="title">
-        <select id='select_for_title' onChange="fontTitleChange();" class="col-sm-3"></select>
-        <input type="number" id="title_size" min="0" max="70" value="30" onchange="updateSize()" class="col-sm-2"/>
-        <input type="color" onchange="updateColor()" id="title_color" class="col-sm-1"/>
+        <input type="text" placeholder="Druga linija naslova" name="title" value="{{$page->title ?? ''}}"
+               class="title col-sm-4" id="title" title="Izmeni drugu liniju naslova">
+        <select id='select_for_title' onChange="fontTitleChange();" class="col-sm-3" title="Promeni font"></select>
+        <input type="number" id="title_size" min="0" max="70" value="30" onchange="updateSize()" class="col-sm-2"
+               title="Promeni veličinu"/>
+        <input type="color" onchange="updateColor()" id="title_color" class="col-sm-1" title="Promeni boju"/>
     </span>
 </span>
 
@@ -21,24 +25,41 @@
     <div class="container">
         <div class="intro">
             <p>Izaberi sliku:</p>
-            <input type="file" name="image" id="image">
+            <input type="file" name="image" id="image" class="btn btn-dark">
             <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="{{asset($page->image ?? 'img/about.jpg')}}"
                  alt="">
             <div class="intro-text text-center bg-faded rounded">
-                <span class="section-heading mb-4">
+                <span class="section-heading mb-5">
                     <span class="">
-                        <input type="text" name="section1_pre_title" placeholder="Pred naslov sekcije"
-                               value="{{$page->section1_pre_title ?? ''}}" id="section1_pre_title" class="col-sm-4">
-                        <select id='select_section1_pre_title' onChange="fontSection1TitleChange();" class="col-sm-4"></select><br>
-                        <input type="number" id="section1_pre_title_size" min="0" max="70" value="15" onchange="updateSize()" class="col-sm-3"/>
+                        <input type="text" name="section1_pre_title" placeholder="Prva linija naslova"
+                               value="{{$page->section1_pre_title ?? ''}}" id="section1_pre_title"
+                               class="section1_pre_title col-sm-4">
+                        <select id='select_section1_pre_title' onChange="fontSection1PreTitleChange();"
+                                class="col-sm-4"></select>
+                        <input type="number" id="section1_pre_title_size" min="0" max="70" value="15"
+                               onchange="updateSize()" class="col-sm-2"/>
                         <input type="color" onchange="updateColor()" id="section1_pre_title_color" class="col-sm-2"/>
-                    </span>
+                    </span><br>
+                    <hr>
                     <span class="">
-                        <input type="text" name="section1_title" placeholder="Naslov sekcije"
-                               value="{{$page->section1_title ?? ''}}">
+                        <input type="text" name="section1_title" placeholder="Druga linija naslova"
+                               value="{{$page->section1_title ?? ''}}" id="section1_title"
+                               class="section1_title col-sm-4">
+                        <select id='select_section1_title' onChange="fontSection1TitleChange();"
+                                class="col-sm-4"></select><br>
+                        <input type="number" id="section1_title_size" min="0" max="70" value="15"
+                               onchange="updateSize()" class="col-sm-2"/>
+                        <input type="color" onchange="updateColor()" id="section1_title_color" class="col-sm-2"/>
                     </span>
-                </span>
-                <textarea type="text" name="section1_text" placeholder="Tekst sekcije" cols="30" rows="10">{{$page->section1_text ?? ''}}</textarea>
+                </span><br>
+                <hr>
+                <textarea type="text" name="section1_text" placeholder="Tekst sekcije" cols="30" rows="10"
+                          id="section1_text" class="section1_text col-sm-4">
+                    {{$page->section1_text ?? ''}}</textarea>
+                <select id='select_section1_text' onChange="fontSection1TitleChange();" class="col-sm-4"></select><br>
+                <input type="number" id="section1_text" min="0" max="70" value="15" onchange="updateSize()"
+                       class="col-sm-2"/>
+                <input type="color" onchange="updateColor()" id="section1_text_color" class="col-sm-2"/>
             </div>
         </div>
     </div>
